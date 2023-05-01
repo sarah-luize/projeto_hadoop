4fcab6708a3b85b7882e1ba03b55d08be03e9d37
Giancarlo Lester <gatobugado.dev@gmail.com>
2023-05-01 10:34:00 -0300

    Melhorias no script de execução

--- src/run.sh
+++ src/run.sh
@@ -1,13 +1,17 @@
-# Donload do mês atual
-python src/download_data.py
+# Donload dos dados do mês atual
+# python src/download_data.py
 
 # Download completo dos dados
 python src/download_data.py --full
 
+# Enviar os dados para dentro do docker do Namenode
 docker compose cp files/extract/. namenode:/home/funds/
 docker compose cp files/zip/. namenode:/home/funds_bkp/
 
-docker compose exec -it namenode bash
+# Transferir os dados do docker para os datanodes
+docker compose exec -it namenode bash -c "hdfs dfs -put -f /home/funds /"
+docker compose exec -it namenode bash -c "hdfs dfs -put -f /home/funds_bkp /"
 
-hdfs dfs -put -f /home/funds /
-hdfs dfs -put -f /home/funds_bkp /
+# Limpar os dados
+# docker compose exec -it namenode bash -c "hdfs dfs -rmr /funds_bkp"
+# docker compose exec -it namenode bash -c "hdfs dfs -rmr /funds"
